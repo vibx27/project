12552902829dc348de6942a43876e5d5fb246017
Bruno Artur <batlopes@yahoo.com.br>
2023-01-10 18:16:07 -0300

    Creating base layout

--- apps/frontend/components.d.ts
+++ apps/frontend/components.d.ts
@@ -7,9 +7,31 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    ABreadcrumb: typeof import('ant-design-vue/es')['Breadcrumb']
+    ABreadcrumbItem: typeof import('ant-design-vue/es')['BreadcrumbItem']
     AButton: typeof import('ant-design-vue/es')['Button']
+    ALayout: typeof import('ant-design-vue/es')['Layout']
+    ALayoutContent: typeof import('ant-design-vue/es')['LayoutContent']
+    ALayoutFooter: typeof import('ant-design-vue/es')['LayoutFooter']
+    ALayoutHeader: typeof import('ant-design-vue/es')['LayoutHeader']
+    ALayoutSider: typeof import('ant-design-vue/es')['LayoutSider']
+    AMenu: typeof import('ant-design-vue/es')['Menu']
+    AMenuItem: typeof import('ant-design-vue/es')['MenuItem']
+    APageHeader: typeof import('ant-design-vue/es')['PageHeader']
     ASpace: typeof import('ant-design-vue/es')['Space']
+    ASubMenu: typeof import('ant-design-vue/es')['SubMenu']
+    DesktopOutlined: typeof import('@ant-design/icons-vue')['DesktopOutlined']
+    FileOutlined: typeof import('@ant-design/icons-vue')['FileOutlined']
+    LaptopOutlined: typeof import('@ant-design/icons-vue')['LaptopOutlined']
+    MenuFoldOutlined: typeof import('@ant-design/icons-vue')['MenuFoldOutlined']
+    MenuUnfoldOutlined: typeof import('@ant-design/icons-vue')['MenuUnfoldOutlined']
+    NotificationOutlined: typeof import('@ant-design/icons-vue')['NotificationOutlined']
+    PieChartOutlined: typeof import('@ant-design/icons-vue')['PieChartOutlined']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    TeamOutlined: typeof import('@ant-design/icons-vue')['TeamOutlined']
+    UploadOutlined: typeof import('@ant-design/icons-vue')['UploadOutlined']
+    UserOutlined: typeof import('@ant-design/icons-vue')['UserOutlined']
+    VideoCameraOutlined: typeof import('@ant-design/icons-vue')['VideoCameraOutlined']
   }
 }
--- apps/frontend/src/modules/home/views/Home.vue
+++ apps/frontend/src/modules/home/views/Home.vue
@@ -1,7 +1,248 @@
-<script setup lang="ts">
-  import HomeComponent from '../components/Home.vue';
+<script lang="ts">
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  LaptopOutlined,
+  NotificationOutlined
+} from '@ant-design/icons-vue';
+import { defineComponent, ref } from 'vue';
+
+
+
+export default defineComponent({
+  components: {
+    UserOutlined,
+    LaptopOutlined,
+    NotificationOutlined,
+    MenuUnfoldOutlined,
+    MenuFoldOutlined
+  },
+  setup() {
+    return {
+      selectedKeys1: ref<string[]>(['2']),
+      selectedKeys2: ref<string[]>(['1']),
+      collapsed: ref<boolean>(true),
+      openKeys: ref<string[]>(['sub1']),
+      routes: ref<object[]>([
+        {
+          path: 'index',
+          breadcrumbName: 'Page 1',
+        },
+        {
+          path: 'first',
+          breadcrumbName: 'Page 2',
+        },
+        {
+          path: 'second',
+          breadcrumbName: 'Page 3',
+        },
+      ])
+    };
+  },
+});
 </script>
 
+
 <template>
-  <HomeComponent />
+  <div style="position:relative;">
+    <a-layout style="min-height: 100%" has-sider>
+
+      <div style="width: 48px; overflow: hidden; transition: width .2s"></div>
+      <a-layout-sider v-model:collapsed="collapsed" collapsible theme="light"  style="overflow: hidden; max-width: 48px; min-width: 48px; width: 48px; flex: 0 0 48px; position: fixed; top: 0; left: 0; z-index: 10; height: 100%;">
+        <template v-slot:trigger>
+          <menu-unfold-outlined
+              v-if="collapsed"
+              class="trigger"
+          />
+          <menu-fold-outlined v-else class="trigger"/>
+        </template>
+        <div class="logo" />
+        <a-menu v-model:selectedKeys="selectedKeys" mode="inline">
+          <a-sub-menu key="sub1">
+            <template #title>
+            <span>
+              <user-outlined />
+              <span>User</span>
+            </span>
+            </template>
+            <a-menu-item key="3">Tom</a-menu-item>
+            <a-menu-item key="4">Bill</a-menu-item>
+            <a-menu-item key="5">Alex</a-menu-item>
+          </a-sub-menu>
+          <a-menu-item key="1">
+            <user-outlined />
+            <span>nav 1</span>
+          </a-menu-item>
+          <a-menu-item key="2">
+            <video-camera-outlined />
+            <span>nav 2</span>
+          </a-menu-item>
+          <a-menu-item key="3">
+            <upload-outlined />
+            <span>nav 3</span>
+          </a-menu-item>
+        </a-menu>
+
+      </a-layout-sider>
+      <a-layout style="width: calc(100% - 48px)">
+<!--          <a-layout-header class="header">-->
+<!--            <div class="logo" />-->
+<!--          </a-layout-header>-->
+        <a-page-header
+            style="border: 1px solid rgb(235, 237, 240)"
+            :breadcrumb="{ routes }"
+        >
+          <template v-slot:title>
+            <span style="font-size: 2rem">Title</span>
+          </template>
+        </a-page-header>
+        <!--      <a-breadcrumb style="margin: 16px 0">-->
+        <!--        <a-breadcrumb-item>Home</a-breadcrumb-item>-->
+        <!--        <a-breadcrumb-item>List</a-breadcrumb-item>-->
+        <!--        <a-breadcrumb-item>App</a-breadcrumb-item>-->
+        <!--      </a-breadcrumb>-->
+        <a-layout-content
+            :style="{margin:'0 24px', padding: '24px', background: '#fff', minHeight: '280px' }"
+        >
+          <ul>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+          </ul>
+        </a-layout-content>
+      </a-layout>
+    </a-layout>
+  </div>
 </template>
+
+<style>
+.header{
+  background-color: #001529;
+}
+
+/*#components-layout-demo-custom-trigger .trigger {*/
+/*  font-size: 18px;*/
+/*  line-height: 64px;*/
+/*  padding: 0 24px;*/
+/*  cursor: pointer;*/
+/*  transition: color 0.3s;*/
+/*}*/
+
+/*#components-layout-demo-custom-trigger .trigger:hover {*/
+/*  color: #1890ff;*/
+/*}*/
+
+/*#components-layout-demo-custom-trigger .logo {*/
+/*  height: 32px;*/
+/*  background: rgba(255, 255, 255, 0.3);*/
+/*  margin: 16px;*/
+/*}*/
+
+/*.site-layout .site-layout-background {*/
+/*  background: #fff;*/
+/*}*/
+
+/*.site-layout-background {*/
+/*  background: #fff;*/
+/*}*/
+</style>
